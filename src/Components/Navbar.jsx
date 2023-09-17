b3bdb350f52f26930d121c77630c04ce99dc1582
LASGLOWTECH <omozemojeau@gmail.com>
2023-09-17 15:59:52 -0700

    added workflow

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,13 +2,20 @@
 import { useState } from "react";
 
 import { AiOutlineClose } from "react-icons/ai"
-
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from "react";
 
 import {GiHamburgerMenu} from "react-icons/gi"
 import { Link } from "react-router-dom";
 
 import { IoMdArrowDropdown, IoMdArrowDropup } from "react-icons/io"
 const Navbar = () => {
+  
+  useEffect(() => {
+    AOS.init({ duration: 2000 });
+
+}, []);
 
 
   const [isOpen3, setisOpen3] = useState(false)
@@ -58,17 +65,17 @@ const Navbar = () => {
 
 
 
-      <div className='Nav  w-100v  bg-darkBlue '>
-        <div className=" xs:px-4 mx-auto container flex flex-row py-2 justify-between items-center ">
+      <div className='Nav border border-b-black-200 md:px-20 w-100vh  bg-white ' data-aos="slide-up">
+        <div className=" xs:px-4   mx-auto  flex flex-row py-2 justify-between items-center ">
           <div className="navbrand">
             <img src="/50rein.png" alt="vita" className="w-[200px] h-auto " />
 
           </div>
 
-          <div className="hidden LIinksmd md:flex pt-3 font-medium ms-32" >
-            <Link to="/" className="text-base px-4  hover:border-b-2 hover:rounded-l transition border-orangeRed  text-white">Home</Link>
+          <div className="hidden LIinksmd md:flex pt-4 font-medium ms-32" >
+            <Link to="/" className="text-base px-4  hover:border-b-2 hover:rounded-l transition text-base  border-orangeRed  text-lightBlue font-bold">Home</Link>
 {/* drop items main header */}
-            <div className="drophead text-base px-4   relative transition duration-500 ease-out border-orangeRed  text-white " >
+            <div className="drophead text-base px-4   relative transition duration-500 ease-out border-orangeRed  text-lightBlue font-bold" >
 
 
               <span className="flex flex-row justify-start cursor items-center"  onClick={openItems}>Who we are
@@ -80,20 +87,20 @@ const Navbar = () => {
               <div className=" flex flex-col"  >
 
                 {isOpen && (
-                  <div className="dropItems  flex flex-col absolute  transition duration-500 ease-out  w-[150px]  mt-4 bg-white"  onClick={openItems}  onMouseLeave={openItems}>
-                    <Link to="/About" className="text-base px-6  pt-2  hover:text-orangeRed  text-darkBlue">About us
+                  <div className="dropItems  flex flex-col absolute  transition duration-500 ease-out  w-[150px]  mt-3 bg-white"  onClick={openItems}  onMouseLeave={openItems}>
+                    <Link to="/About" className="text-base px-6  pt-2  hover:text-orangeRed  text-lightBlue font-medium ">About us
                     </Link>
 
                     {/* drop items sumheader */}
-                    <span className="flex flex-row px-3 text-darkBlue pt-2 justify-start items-center" onClick={openItems2}  >Our People
+                    <span className="flex flex-row px-3 text-lightBlue  font-medium pt-2 justify-start items-center" onClick={openItems2}  >Our People
 
                       {!isOpen2 ? (<IoMdArrowDropdown className="text-orangeRed text-base" />) : (<IoMdArrowDropup className="text-orangeRed text-base" />)} </span>
 
                     {isOpen2 && (
                       <div className=" flex flex-col bg-white">
-                        <Link to="/Team" className="text-base px-3  pt-2  hover:text-orangeRed  text-darkBlue">Our Team
+                        <Link to="/Team" className="text-base px-3  pt-2  hover:text-orangeRed  text-lightBlue font-medium">Our Team
                         </Link>
-                        < Link to="/About" className="text-base px-3  pt-2  hover:text-orangeRed  text-darkBlue">Testimonials
+                        < Link to="/About" className="text-base px-3  pt-2  hover:text-orangeRed  text-lightBlue font-medium">Testimonials
                         </Link>
                       </div>
                     )}
@@ -111,15 +118,15 @@ const Navbar = () => {
 
 
 
-            <Link to="/services" className="text-base px-3   hover:border-b-2 transition  border-orangeRed text-white ">What We Do</Link>
+            <Link to="/services" className="text-base px-3   hover:border-b-2 transition  border-orangeRed text-lightBlue  font-bold">What We Do</Link>
 
 
 
 
-            <div className="drophead text-base px-3   relative transition duration-500 ease-out border-orangeRed  text-white "  >
+            <div className="drophead text-base px-3   relative transition duration-500 ease-out border-orangeRed text-lightBlue   "  >
 
 
-<span className="flex flex-row justify-start cursor items-center" onClick={openItems3}>Keep Up With Us
+<span className="flex flex-row justify-start cursor font-bold items-center" onClick={openItems3}>Keep Up With Us
 
   {!isOpen3 ? (<IoMdArrowDropdown className="text-orangeRed text-base" />) :
     (<IoMdArrowDropup className="text-orangeRed text-base" />)
@@ -128,12 +135,12 @@ const Navbar = () => {
 <div className=" flex flex-col h-auto py-2"   >
 
   {isOpen3 && (
-    <div className="dropItems flex flex-col absolute  transition duration-500 ease-out  w-[150px]  mt-6 bg-white" onMouseLeave={openItems3} onClick={openItems3}>
-      <Link to="/realtors" className="text-base px-3  pt-2  hover:text-orangeRed  text-darkBlue">Realtors
+    <div className="dropItems flex flex-col absolute  transition duration-500 ease-out  w-[150px]  mt-3 bg-white" onMouseLeave={openItems3} onClick={openItems3}>
+      <Link to="/realtors" className="text-base px-3  pt-2  hover:text-orangeRed   text-lightBlue">Realtors
       </Link>
-      <Link to="/Team" className="text-base px-3   hover:text-orangeRed transition    text-darkBlue ">Photo gallery</Link>
-      <Link to="/Team" className="text-base px-3   hover:text-orangeRed transition    text-darkBlue ">Volunteer</Link>
-      <Link to="/Team" className="text-base px-3   hover:text-orangeRed transition    text-darkBlue ">Partners</Link>
+      <Link to="/Team" className="text-base px-3   hover:text-orangeRed transition    text-lightBlue ">Photo gallery</Link>
+      <Link to="/Team" className="text-base px-3   hover:text-orangeRed transition    text-lightBlue ">Volunteer</Link>
+      <Link to="/Team" className="text-base px-3   hover:text-orangeRed transition    text-lightBlue ">Partners</Link>
 
      
 
@@ -152,7 +159,7 @@ const Navbar = () => {
 
 
           </div>
-          <Link to="/contact" className=" hidden text-base px-3 py-2 rounded-md font-bold bg-orangeRed text-white md:flex ">
+          <Link to="/contact" className=" hidden text-base px-3 py-2 hover:bg-lightBlue rounded-md font-bold bg-orangeRed text-white md:flex ">
             <button className=" ">Contact us</button> </Link>
 
           {/* CONTROLS */}
--- src/Components/hero.jsx
+++ src/Components/hero.jsx
@@ -11,44 +11,47 @@ const Home = () => {
   useEffect(() => {
     AOS.init({ duration: 1000 });
 
-}, []);
+  }, []);
   return (
     <>
-      <div className="bg-blue-50 h-auto w-100 "></div>
 
 
-      <div className=" Hero py-2  h-auto w-100 ">
+
+      <div className=" Hero py-10 h-auto w-100 ">
         <div className="  container py-6 mx-auto flex flex-col items-between md:flex-row ">
-          <div className="left px-4 py-6  "  data-aos="slide-right">
+          <div className="left px-4 py-6  max-width-medium  " data-aos="slide-right">
 
-            <h1 className="text-darkBlue pt-5 text-4xl  md:text-left md:text-5xl   max-w-xl font-bold">A Compedium of Excellence </h1>
-            <p className="text-[18px] font-normal text-greyBlack pt-6  pb-6">
-           50 Rein is curating an anthology of greatness in a compedium. A compedium where the journeys of Nigerian Real Estate Icons can be explored, their challenges understood, and their triumphs celebrated.
- </p>
+            <h1 className="text-darkBlue text-center pt-5 text-5xl  md:text-left md:text-5xl max-w-lg   font-bold">A Compedium of Excellence </h1>
+            <p className="text-[18px] text-center md:text-left font-normal text-greyBlack pt-6  pb-10">
+              50 REIN is curating an anthology of greatness in a compedium. A compedium where the journeys of Nigerian Real Estate Icons can be explored, their challenges understood, and their triumphs celebrated.
+            </p>
 
-            <Link to="/About" className="  text-xl 
-px-3 py-3 rounded-md  bg-orangeRed text-white hover:bg-lightBlue  ">
+            <Link to="/About" className=" mx-auto text-xl 
+px-3 py-3 mt-8 rounded-md  bg-orangeRed text-white hover:bg-lightBlue  ">
               <button className=" text-xl">Learn more</button> </Link>
 
           </div>
-          <div className="  relative flex flex-col justify-between items-center  py-6 "  data-aos="zoom-in-up">
+
+
+          <div className="mt-20 px-3 bg-white  " data-aos="zoom-in-up">
+            <img src="/services.jpg" alt="vita"  className="  w-full rounded-br-[30px] rounded-tl-[30px] md:w-[1500px] " />
+
         
-      
-            <div className="right w-[300px] h-[300px] animate-spin-3s flex items-center justify-center rounded-circle bg-orangeRed rounded-[100%]">
 
-              <div className="rightcircle w-[290px] h-[290px] rounded-circle rounded-[100%]">
-
-
-
-              </div>
-
-
-            </div>
-
-           
-            
 
           </div>
+
+          {/* <div className="  relative flex flex-col mt-6 justify-between bg-green-300 h-[300] w-full rounded-br-[30px] rounded-tl-[30px] items-center  py-6 " data-aos="zoom-in-up">
+
+          <img src="/services.jpg" alt="vita"  className="w-full  rounded-br-[30px] rounded-tl-[30px] h-auto " />
+
+        
+
+
+
+
+
+          </div> */}
 
 
         </div>
@@ -57,17 +60,17 @@ px-3 py-3 rounded-md  bg-orangeRed text-white hover:bg-lightBlue  ">
       </div>
 
 
-      <section className="bg-green-50">
+      <section className="">
         <div className="  container py-6 mx-auto grid  gap-3 grid-cols-1    md:grid-cols-2 ">
-          <div className=" bg-cyanGreen shadow-lg rounded-md " data-aos="zoom-in-up">
+          <div className="   rounded-md " data-aos="zoom-in-up">
             <div className="left px-4 py-12  ">
 
-              <h4 className="text-darkBlue text-2xl  font-bold">Who we are</h4>
-              <div className="border-2 mt-2 border-orangeRed rounded-md w-[150px] "></div>
-              <p className="text-l text-greyBlack  text-base text-truncate font-md py-6 ">
-              50 REIN is an ideation with it’s core values and mandate encompass in amplifying the 
-              real estate industry in Nigeria and projecting Nigeria real estate Icons to the world hence celebrating their unique heritage in professionalism,
-               integrity, consistency, morals and delivery of values....  </p>
+              <h3 className="text-lightBlue text-3xl  font-bold">Who we are</h3>
+              <div className="border-2 mt-2 border-orangeRed rounded-md w-[100px] "></div>
+              <p className="text-[18px]  text-greyBlack   text-truncate font-md py-6 ">
+                50 REIN is an ideation with it’s core values and mandate encompass in amplifying the
+                real estate industry in Nigeria and projecting Nigeria real estate Icons to the world hence celebrating their unique heritage in professionalism,
+                integrity, consistency, morals and delivery of values.  </p>
 
               <Link to="/About" className="  text-xl 
 px-2 py-2 rounded-md  bg-orangeRed text-white hover:bg-lightBlue  ">
@@ -78,16 +81,16 @@ px-2 py-2 rounded-md  bg-orangeRed text-white hover:bg-lightBlue  ">
 
           </div>
 
-          <div className=" bg-white shadow-lg  "  data-aos="zoom-in-up">
-          <div className="left px-4 py-12  ">
+          <div className=" bg-white  " data-aos="zoom-in-up">
+            <div className="left px-4 py-12  ">
 
-              <h4 className="text-darkBlue text-2xl  font-bold">What we do</h4>
-              <div className="border-2 mt-2 border-orangeRed rounded-md w-[150px] "></div>
-              <p className="text-base  text-greyBlack font-md py-6 ">
-              Being passionate about creating a terrific impact in the Nigeria real estate industry,
-               we strive to inspire future industry leaders by creating a definitive collection that showcases
-                the exemplary achievements of 
-              Real Estate Icons in Nigeria and in turn elevate the nation's real estate reputation globally.  </p>
+              <h4 className="text-lightBlue text-3xl  font-bold">What we do</h4>
+              <div className="border-2 mt-2 border-orangeRed rounded-md w-[100px] "></div>
+              <p className="text-[18px]  text-greyBlack   font-md py-6 ">
+                Being passionate about creating a terrific impact in the Nigeria real estate industry,
+                we strive to inspire future industry leaders by creating a definitive collection that showcases
+                the exemplary achievements of
+                Real Estate Icons in Nigeria and in turn elevate the nation's real estate reputation globally.  </p>
 
               <Link to="/services" className="  text-xl 
 px-3 py-2 rounded-md  bg-orangeRed text-white hover:bg-lightBlue  ">
@@ -98,31 +101,30 @@ px-3 py-2 rounded-md  bg-orangeRed text-white hover:bg-lightBlue  ">
 
           </div>
 
-          <div className=" bg-white shadow-lg  "  data-aos="zoom-in-up">
-          <img src="/services.jpg" alt="vita" className="w-full h-auto " />
 
-          <p className="text-base px-4  text-greyBlack font-md py-6 ">
-                We are group of organized individuals, aiming for the projection
-                of the image of outstanding Real estate companies, who have
-                maintained their integrity over the years  </p>
-      
+
+          <div className="mt-20  bg-white  " data-aos="zoom-in-up">
+            <img src="/services.jpg" alt="vita"  className="w-full  rounded-br-[30px] rounded-tl-[30px] h-auto " />
+
+        
 
 
           </div>
-          <div className=" bg-cyanGreen shadow-lg rounded-md " data-aos="zoom-up">
-            <div className="left px-4 py-12  ">
 
-              <h4 className="text-darkBlue text-2xl  font-bold">How we Started</h4>
-              <div className="border-2 mt-2 border-orangeRed rounded-md w-[150px] "></div>
-              <p className="text-base text-greyBlack  font-md py-6 ">
-              In the dynamic world of real estate, our journey began with a stumble, but it transformed 
-into an unwavering quest for authenticity and dependability. Picture a group of young 
-individuals, brimming with enthusiasm and dreams of conquering the real estate realm. Eager 
-to carve their path, they invested their time, resources, and aspirations into projects that held 
-the promise of success. But the real estate landscape had its lessons to teach, and those 
-lessons came at a cost............
+          <div className=" mt-20 rounded-md " data-aos="zoom-up">
+            <div className="left px-4   ">
 
-  </p>
+              <h2 className="text-darkBlue font-bold text-4xl">How we Started</h2>
+              <div className="border-2 mt-2 border-orangeRed rounded-md w-[100px] "></div>
+              <p className="text-[18px] text-greyBlack  font-md py-6 ">
+                In the dynamic world of real estate, our journey began with a stumble, but it transformed
+                into an unwavering quest for authenticity and dependability. Picture a group of young
+                individuals, brimming with enthusiasm and dreams of conquering the real estate realm. Eager
+                to carve their path, they invested their time, resources, and aspirations into projects that held
+                the promise of success. But the real estate landscape had its lessons to teach, and those
+                lessons came at a cost.
+
+              </p>
 
               <Link to="/About" className="  text-xl 
 px-3 py-2 rounded-md  bg-orangeRed text-white hover:bg-lightBlue  ">
@@ -136,18 +138,18 @@ px-3 py-2 rounded-md  bg-orangeRed text-white hover:bg-lightBlue  ">
 
 
 
-          <div className=" bg-white shadow-lg  "  data-aos="zoom-up">
-          <div className="left px-4 py-6  ">
-          <p className="text-2xl  text-orangeRed font-md py-2">
-             Realtors  </p>
+          <div className=" bg-white mt-20 " data-aos="slide-right">
+            <div className="left px-4   ">
+              <p className="text-2xl  text-orangeRed font-md py-2">
+                Realtors  </p>
               <h2 className="text-darkBlue font-bold text-4xl">Estate Companies That
-                     Have made differences</h2>
-           
-              <p className="text-base  text-greyBlack font-md py-6 ">
-              Being passionate about creating a terrific impact in the Nigeria real estate industry,
-               we strive to inspire future industry leaders by creating a definitive collection that showcases
-                the exemplary achievements of 
-              Real Estate Icons in Nigeria and in turn elevate the nation's real estate reputation globally.  </p>
+                Have made differences</h2>
+
+              <p className="text-[18px]  text-greyBlack font-md py-6 ">
+                Being passionate about creating a terrific impact in the Nigeria real estate industry,
+                we strive to inspire future industry leaders by creating a definitive collection that showcases
+                the exemplary achievements of
+                Real Estate Icons in Nigeria and in turn elevate the nation's real estate reputation globally.  </p>
 
               <Link to="/realtors" className="  text-xl 
 px-3 py-3 rounded-md  bg-orangeRed text-white hover:bg-lightBlue  ">
@@ -159,31 +161,30 @@ px-3 py-3 rounded-md  bg-orangeRed text-white hover:bg-lightBlue  ">
           </div>
 
 
-          <div className=" bg-white shadow-lg  "  data-aos="flip-right">
-          <img src="/mirrrorb.jpg" alt="vita" className="w-full h-auto " />
+          <div className=" bg-white mt-20 " data-aos="flip-right">
+            <img src="/mirrrorb.jpg" alt="vita" className="w-full mt-6 rounded-tl-[30px] rounded-br-[30px] h-auto " />
 
-       
-      
+
+
 
 
           </div>
         </div>
 
-
       </section>
 
-      <h3 className="text-4xl  text-center pt-10 pb-3 font-bold px-6 text-darkBlue ">Testimonials</h3>
+      <h3 className="text-4xl  text-center mt-20 pb-2 font-bold px-6 text-darkBlue ">Testimonials</h3>
+
+<div className="mt-10">
+      <Carouselitem  />
+      </div>
+
+      <div className="md:mx-36 ">
 
 
-<Carouselitem/>
 
-
-<div className="md:mx-36 ">
-
-
-
-<Faq data-aos="flip-up"/>
-</div>
+        <Faq data-aos="flip-up" />
+      </div>
 
     </>
 
